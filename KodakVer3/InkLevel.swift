b6476625db9e7f015b0d8fef0158d5859bb8cb83
Arvin Narte <rejnextdoor@gmail.com>
2017-02-14 13:42:11 +0800

    modify Ink Level Storyboard added InkLevel.swift

---
+++ KodakVer3/InkLevel.swift
@@ -0,0 +1,14 @@
+//
+//  InkLevel.swift
+//  KodakVer3
+//
+//  Created by anarte on 14/02/2017.
+//  Copyright © 2017 jmolas. All rights reserved.
+//
+
+import UIKit
+
+class InkLevel: UIViewController{
+    
+  
+}
