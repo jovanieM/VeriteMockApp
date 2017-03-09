9d344fe84731a0338febddbdac83a5162f84a6e0
Arvin Narte <rejnextdoor@gmail.com>
2017-03-09 08:19:08 +0800

    added: routerconnection.png networkandpasswordswitchnetwork.switch
    modified:
    networkandpasswordmanual.swift
    wifisetupstoryboard

--- KodakVer3/NetworkAndPasswordManual.swift
+++ KodakVer3/NetworkAndPasswordManual.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class NetworkAndPasswordManual: UIViewController, SecurityTypeProtocol{
+class NetworkAndPasswordManual: UIViewController,UITextFieldDelegate ,SecurityTypeProtocol{
     
     @IBOutlet weak var ssidTextField: UITextField!
     @IBOutlet weak var viewPassword: UIView!
     @IBOutlet weak var securityLabel: UILabel!
     
     var securityData: String?
+    var unchecked = true
+    var alert: UIAlertController!
+    @IBOutlet weak var showPasswordLabel: UILabel!
+    @IBOutlet weak var checkBox: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     
     override func viewWillAppear(_ animated: Bool) {
         let navTransition = CATransition()
@@ -32,12 +38,8 @@ class NetworkAndPasswordManual: UIViewController, SecurityTypeProtocol{
         ssidTextField.tintColor = UIColor(red: 255/255, green: 183/255, blue: 0/255, alpha: 1)
         
         viewPassword.isHidden = true
-        
-        //if  self.securityLabel.text == "Open"{
-        //    viewPassword.isHidden = true
-        //}else if self.securityLabel.text == "WEP"{
-        //    viewPassword.isHidden = false
-        //}
+        showPasswordLabel.isHidden = true
+        checkBox.isHidden = true
     }
     
     func setDataFromDisplay(datasent: String){
@@ -51,20 +53,108 @@ class NetworkAndPasswordManual: UIViewController, SecurityTypeProtocol{
         }
     }
     
+    @IBAction func checkBoxAction(_ sender: UIButton) {
+        if unchecked{
+            //checkBox.sendActions(for: .touchUpInside)
+            checkBox.setImage(UIImage(named: "check_box_on"), for: .normal)
+            passwordTextField.isSecureTextEntry = false
+            unchecked = false
+        }else{
+            checkBox.setImage(UIImage(named: "check_box_off"), for: .normal)
+            passwordTextField.isSecureTextEntry = true
+            unchecked = true
+        }
+    }
+    
     func setSecurityRowData(dataRow: String){
         securityLabel.text = dataRow
         
         if dataRow == "Open"{
             viewPassword.isHidden = true
+            showPasswordLabel.isHidden = true
+            checkBox.isHidden = true
         }else if dataRow == "WEP"{
             viewPassword.isHidden = false
+            showPasswordLabel.isHidden = false
+            checkBox.isHidden = false
         }else if dataRow == "WPA/WPA2-PSK MIX"{
             viewPassword.isHidden = false
             securityLabel.adjustsFontSizeToFitWidth = true
+            showPasswordLabel.isHidden = false
+            checkBox.isHidden = false
         }else if dataRow == "WPA2-PSK AES"{
             viewPassword.isHidden = false
+            showPasswordLabel.isHidden = false
+            checkBox.isHidden = false
         }
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if viewPassword.isHidden == true{
+            if textField == ssidTextField{
+                ssidTextField.resignFirstResponder()
+                
+                //check if ssid textfield is empty
+                if ssidTextField.text == ""{
+                    alert = UIAlertController(title: "SSID Input Error", message: "Please input SSID", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:{(action: UIAlertAction!) in
+                        self.alert.dismiss(animated: true, completion: nil)
+                    }))
+                    self.present(alert, animated: true, completion: nil)
+                }
+                
+                //check if security label is empty
+                if securityLabel.text == ""{
+                    alert = UIAlertController(title: "Security not selected", message: "Please select Security", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:{(action: UIAlertAction!) in
+                        self.alert.dismiss(animated: true, completion: nil)
+                    }))
+                    self.present(alert, animated: true, completion: nil)
+                    
+                    ssidTextField.resignFirstResponder()
+                }
+                
+                let vc = self.storyboard?.instantiateViewController(withIdentifier: "SwitchNetwork") as! NetworkAndPasswordSwitchNetwork
+                self.navigationController?.pushViewController(vc, animated: true)
+            }
+        }else if viewPassword.isHidden == false{
+            if textField == ssidTextField{
+                passwordTextField.becomeFirstResponder()
+                
+                if securityLabel.text == "WEP"{
+                    if passwordTextField.text == ""{
+                        alert = UIAlertController(title: "Password Input Error", message: "Please confirm the password. \n - 5 or 13 characters of strings. \n - 10 or 26 characters of HEX strings.", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:{(action: UIAlertAction!) in
+                            self.alert.dismiss(animated: true, completion: nil)
+                        }))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }else if securityLabel.text == "WPA/WPA2-PSK MIX"{
+                    if passwordTextField.text == ""{
+                        alert = UIAlertController(title: "Password Input Error", message: "Please confirm the password. \n - 3 or 63 characters of strings. \n - 64 characters of HEX strings.", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:{(action: UIAlertAction!) in
+                            self.alert.dismiss(animated: true, completion: nil)
+                        }))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+                
+            }else{
+                passwordTextField.resignFirstResponder()
+                
+                let vc = self.storyboard?.instantiateViewController(withIdentifier: "SwitchNetwork") as! NetworkAndPasswordSwitchNetwork
+                self.navigationController?.pushViewController(vc, animated: true)
+            }
+        }
+        return true
+    }
+    
+    @IBAction func transitionToSwitchNetwork(_ sender: Any) {
+    }
+    
+    @IBAction func transitionToSwitchNetwork2(_ sender: Any) {
+    }
+    
     
 }
 
---
+++ KodakVer3/NetworkAndPasswordSwitchNetwork.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkAndPasswordSwitchNetwork.swift
+//  KodakVer3
+//
+//  Created by anarte on 08/03/2017.
+//  Copyright © 2017 jmolas. All rights reserved.
+//
+
+import UIKit
+
+class NetworkAndPasswordSwitchNetwork: UIViewController {
+
+    var alert:UIAlertController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    func loadAlert(){
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
